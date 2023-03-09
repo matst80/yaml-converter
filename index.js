08d8349dccbf896d8375c200efae067cf5e2fb5d
matst80 <mats.tornberg@gmail.com>
2023-03-09 11:54:46 +0100

    slask

---
+++ index.js
@@ -0,0 +1,38 @@
+const { readFileSync } = require('fs')
+const http = require('http')
+const { parse } = require('yaml')
+
+const getBoundary = (contentTypeHeader) => {
+	const [_, boundaryOptions] = contentTypeHeader.split('; ')
+	return '--' + boundaryOptions.split('=')[1]
+}
+
+const getFileChunks = (req, boundary) => new Promise((res) => {
+	let body = []
+	req.on('data', (chunk) => {
+		body.push(chunk)
+	}).on('end', () =>
+		res(Buffer.concat(body).toString().split(boundary).filter(chunk => chunk && chunk.indexOf('filename=') > 0).map((chunk) => {
+			return chunk.split('\r\n\r\n')[1]
+		}))
+	)
+})
+
+const splitYaml = (chunks) => chunks.map(chunk => chunk.split('---')).flat()
+
+const convertToJSON = (chunks) => chunks.map((chunk) => {
+	return parse(chunk)
+})
+
+const index = readFileSync('index.html', 'utf8')
+
+http.createServer(function (req, res) {
+	if (req.method === 'POST') {
+		return getFileChunks(req, getBoundary(req.headers['content-type'])).then(splitYaml).then(convertToJSON).then((json) => {
+			res.writeHead(200, { 'Content-Type': 'application/json' })
+			res.end(JSON.stringify(json, null, 2))
+		})
+	} else {
+		return res.writeHead(200, { 'Content-Type': 'text/html' }).end(index)
+	}
+}).listen(8080)
