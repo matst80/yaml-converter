2440bbf7a25a0022646aae43bd2b5bf73fd2ac28
matst80 <mats.tornberg@gmail.com>
2023-03-10 16:59:35 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
-const { readFileSync } = require('fs')
-const { getBoundary, getFileChunks, splitYaml, convertToJSON, asDeployments } = require('./utils')
-const http = require('http')
-const port = process.env.PORT ?? '8080'
-const index = readFileSync('index.html', 'utf8')
+const { readFileSync } = require("fs");
+const { getBoundary, getFileChunks, splitYaml } = require("./utils");
+const http = require("http");
+const port = process.env.PORT ?? "8080";
+const index = readFileSync("index.html", "utf8");
 
-const asDownload = (res) => (json) => {
-	res.writeHead(200, { 'Content-Type': 'application/json', 'Content-Disposition': 'attachment; filename=result.json' })
-	res.end(JSON.stringify(json, asDeployments, 2))
-}
+const asDownload = (res) => (content) => {
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "Content-Disposition": "attachment; filename=result.js",
+  });
+  res.end(content);
+};
 
-http.createServer(function (req, res) {
-	if (req.method === 'POST') {
-		const boundary = getBoundary(req.headers['content-type'])
-		return getFileChunks(req, boundary)
-			.then(splitYaml)
-			.then(convertToJSON)
-			.then(asDownload(res))
-	} else {
-		return res.writeHead(200, { 'Content-Type': 'text/html' }).end(index)
-	}
-}).listen(Number(port))
-console.log(`Server running at port ${port}`)
+const toPulumi = ({ kind, apiVersion, metadata, ...data }) => {
+  const { name, namespace, ...meta } = metadata;
+  return `new ${kind}("${name}",${JSON.stringify(
+    {
+      metadata: { name, namespace: "REMOVE", ...meta },
+      ...data,
+    },
+    null,
+    2
+  ).replace('space": "REMOVE"', 'space"')},options)`;
+};
+
+const toSlask = ({ kind, apiVersion, metadata, ...data }) => {
+  const { name, namespace, ...meta } = metadata;
+  return `create${kind}(namespace,${JSON.stringify(
+    {
+      metadata: { name, ...meta },
+      ...data,
+    },
+    null,
+    2
+  )})`;
+};
+
+http
+  .createServer(function (req, res) {
+    if (req.method === "POST") {
+      const boundary = getBoundary(req.headers["content-type"]);
+      return getFileChunks(req, boundary)
+        .then(splitYaml)
+        .then(({ files, fields }) => {
+          const { format } = fields;
+          if (format === "pulumi") {
+            return files.map(toPulumi).join("\n");
+          } else if (format === "slask") {
+            return files.map(toSlask).join("\n");
+          }
+          return JSON.stringify(files, null, 2);
+        })
+        .then(asDownload(res));
+    } else {
+      res.setHeader("Cache-Control", "public; max-age=604800");
+      return res.writeHead(200, { "Content-Type": "text/html" }).end(index);
+    }
+  })
+  .listen(Number(port));
+console.log(`Server running at port ${port}`);
--- utils.js
+++ utils.js
@@ -1,42 +1,70 @@
-const { parse } = require('yaml')
+const { parse } = require("yaml");
 
 const getBoundary = (contentTypeHeader) => {
-	const [_, boundaryOptions] = contentTypeHeader.split('; ')
-	return '--' + boundaryOptions.split('=')[1]
-}
+  const [_, boundaryOptions] = contentTypeHeader.split("; ");
+  return "--" + boundaryOptions.split("=")[1];
+};
 
-const getFileChunks = (req, boundary) => new Promise((res) => {
-	let body = []
-	req.on('data', (chunk) => {
-		body.push(chunk)
-	}).on('end', () =>
-		res(Buffer.concat(body).toString().split(boundary).filter(chunk => chunk && chunk.indexOf('filename=') > 0).map((chunk) => {
-			return chunk.split('\r\n\r\n')[1]
-		}))
-	)
-})
+const extractKeyValue = (p) => {
+  console.log("hoho", p);
+  const { headers, content } = p;
+  const match = headers?.match(/name="(.*)"/);
+  return match?.length ? [match[1], content.trim()] : null;
+};
 
-const splitYaml = (chunks) => chunks.map(chunk => chunk.split('---')).flat()
+const isField = ({ headers }) =>
+  headers?.length &&
+  headers?.match(/name="(.*)"/) &&
+  headers.indexOf("filename=") === -1;
 
-const convertToJSON = (chunks) => chunks.filter(chunk => chunk != null).map((chunk) => {
-	const { kind, apiVersion, metadata, ...data } = parse(chunk)
-	const { name, namespace, ...meta } = metadata
-	return {
-		kind,
-		apiVersion,
-		metadata: { name, ...meta },
-		...data
-	}
-})
+const isFile = ({ headers, content }) =>
+  content?.length > 10 && headers?.indexOf("filename=") > 0;
 
-function asDeployments(key, value) {
-	return value
-}
+const getFileChunks = (req, boundary) =>
+  new Promise((res) => {
+    let body = [];
+    req
+      .on("data", (chunk) => {
+        body.push(chunk);
+      })
+      .on("end", () => {
+        const parts = Buffer.concat(body)
+          .toString()
+          .split(boundary)
+          .map((chunk) => {
+            [headers, content] = chunk.split("\r\n\r\n");
+            return { headers, content };
+          });
+
+        const files = parts.filter(isFile).map((d) => d.content);
+        const fields = Object.fromEntries(
+          parts.filter(isField).map(extractKeyValue)
+        );
+        res({ files, fields });
+      });
+  });
+
+const toJson = (chunk) => {
+  const { kind, apiVersion, metadata, ...data } = parse(chunk);
+  const { name, namespace, ...meta } = metadata;
+  return {
+    kind,
+    apiVersion,
+    metadata: { name, ...meta },
+    ...data,
+  };
+};
+
+const splitYaml = ({ files, fields }) => ({
+  files: files
+    .map((chunk) => chunk.split("---"))
+    .flat()
+    .map(toJson),
+  fields,
+});
 
 module.exports = {
-	getBoundary,
-	getFileChunks,
-	splitYaml,
-	convertToJSON,
-	asDeployments
-}
+  getBoundary,
+  getFileChunks,
+  splitYaml,
+};
